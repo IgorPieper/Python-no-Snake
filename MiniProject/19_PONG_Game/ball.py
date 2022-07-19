203612acb5207e22e9c6c972913de1aa818a1d82
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2022-07-19 23:02:49 +0200

    Create ball.py

---
+++ MiniProject/19_PONG_Game/ball.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.color("white")
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        x_pos = self.xcor() + self.x_move
+        y_pos = self.ycor() + self.y_move
+        self.goto(x_pos, y_pos)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset(self):
+        self.goto(0, 0)
